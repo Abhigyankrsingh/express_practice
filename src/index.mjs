51ececf166c11fde68b4ca190ccf0a9461219895
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-07 23:21:36 +0530

    Code_Updated

--- src/index.mjs
+++ src/index.mjs
@@ -3,4 +3,9 @@ import express from 'express';
 const app = express();
 
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>{
+
+    console.log(`Running on Port ${PORT}`);
+});
